684be9ac0960ce5fb5cd8f56e643e19126eab52e
Matheus Pereira <matheus.pereira@scaffoldeducation.com.br>
2021-03-16 11:39:46 -0300

    Arquivos de migração e service provider

---
+++ src/JwtManagerServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace JwtManager;
+
+use Illuminate\Support\ServiceProvider;
+
+class JwtManagerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->registerMigrations();
+        }
+    }
+
+    /**
+     * Register Passport's migration files.
+     *
+     * @return void
+     */
+    protected function registerMigrations():void
+    {
+        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+
+     	$this->publishes([
+            __DIR__.'/../database/migrations' => database_path('migrations'),
+        ], 'jwtmanager-migrations');
+    }
+
+}
